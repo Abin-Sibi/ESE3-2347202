a36f7559450dd053d4697eeb9db56ad04c260977
AbinSibi466 <abin.sibi@mca.christuniversity.in>
2024-04-16 11:26:16 +0530

    first commit3

--- Text-Similarity-Analysis.py
+++ Text-Similarity-Analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import nltk
+nltk.download('punkt')
 import streamlit as st
 from nltk.tokenize import word_tokenize,sent_tokenize
 from nltk.corpus import stopwords
